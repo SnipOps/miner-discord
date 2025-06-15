168770c0233c41b674080989567a8e1d1b9e7829
SnipOps <113144800+SnipOps@users.noreply.github.com>
2025-06-15 18:55:17 +0200

    Update keep_alive.py
    update

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,17 @@
-# keep_alive.py
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask(__name__)
+app = Flask('')
 
-@app.route('')
-def home()
-    return Le bot est en ligne !
+@app.route('/')
+def home():
+    return "Bot is alive!"
 
-def run()
-    app.run(host='0.0.0.0', port=8080)
+def run():
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
 
-def keep_alive()
+def keep_alive():
     t = Thread(target=run)
     t.start()
